c48480ddb0dea9a1e3480c9cdc514cbb5a35f69d
DemondAngel <iarogo@hotmail.com>
2023-03-05 16:27:05 -0600

    chore: Organización de Carpetas

---
+++ JavaCliente/Mensaje/JavaClientCode.java
@@ -0,0 +1,39 @@
+package Mensaje;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+class JavaClientCode {
+    public static void main(String[] args) {
+
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Escribe un mensaje ");
+            // String ip ;
+            // String p;
+            String ss;
+            int port;
+
+            // ip = br.readLine();
+            // p = br.readLine();
+            // port = Integer.parseInt(p);
+            Socket s = new Socket("192.168.0.13", 5000);
+            OutputStream os = s.getOutputStream();
+            PrintWriter pw = new PrintWriter(os);
+
+            Scanner in = new Scanner(System.in);
+            ss = in.nextLine();
+            pw.print(ss);
+            pw.flush();
+            pw.close();
+        } catch (Exception e) {
+            System.out.println("ERROR!!!");
+            e.printStackTrace();
+            e.getLocalizedMessage();
+        }
+    }
+}
